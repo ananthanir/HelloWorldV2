61578587c4d684644479973cf9d90b05378625ae
Ananthan <ananthan.rajasekharan@gmail.com>
2022-12-28 22:51:13 +0530

    first push

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
-# Hello World - V2
+## What's this?
 
-My current profile page.
+[VS Code](https://code.visualstudio.com/) inspired developer portfolio app.
+
+## How to ... run locally?
+
+Make sure you have [Node](https://nodejs.org/en/) and [git](https://git-scm.com/) installed.
+
+- Clone the repo:
+
+  ```bash
+  git clone https://github.com/ananthanir/HelloWorldV2
+  cd HelloWorldV2
+  ```
+
+- Install the dependencies:
+
+  ```bash
+  npm install
+  ```
+
+- Fire up a development server:
+
+  ```bash
+  npm start
+  ```
+
+#### Credits
+
+- https://github.com/noworneverev/react-vscode-portfolio
+- https://github.com/caglarturali/caglarturali.github.io
---
+++ src/app/pages/links.tsx
@@ -0,0 +1,34 @@
+import {
+  FaGithub,
+  FaLinkedin,
+  FaTwitter,
+  FaTelegram
+} from "react-icons/fa";
+
+export const links = [
+  {
+    index: 0,
+    title: "Find me on Github",
+    href: "https://github.com/ananthanir",
+    icon: <FaGithub />,
+  },
+  {
+    index: 1,
+    title: "Find me on LinkedIn",
+    href: "https://www.linkedin.com/in/id-ananthan/",
+    icon: <FaLinkedin />,
+  },
+  {
+    index: 2,
+    title: "Follow me on Twitter",
+    href: "https://twitter.com/id_ananthan",
+    icon: <FaTwitter />,
+  },
+  {
+    index: 3,
+    title: "Chat with me on Telegram",
+    href: "https://t.me/ananthanir",
+    icon: <FaTelegram />,
+  }
+  
+];
